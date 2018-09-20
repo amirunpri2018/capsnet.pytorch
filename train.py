e4ddfb91711113d1380745e7cdf993929be84fa0
sakhawathsumit <sakhawathosainsumit@gmail.com>
2018-09-20 17:07:10 +0600

    add files

---
+++ train.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import time
+import argparse
+from tqdm import tqdm
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.optim import Adam
+from torchvision import transforms
+
+from model import CapsNet
+
+
+parser = argparse.ArgumentParser(description='CapsNet training')
+parser.add_argument('--train-manifest', metavar='DIR',
+                    help='path to train manifest csv', default='data/train_manifest.csv')
+parser.add_argument('--val-manifest', metavar='DIR',
+                    help='path to validation manifest csv', default='data/val_manifest.csv')
+parser.add_argument('--img-height', default=128, type=int, help='Image height')
+parser.add_argument('--img-width', default=128, type=int, help='Image width')
+parser.add_argument('--batch-size', default=32, type=int, help='Batch size for training')
+parser.add_argument('--num-workers', default=4, type=int, help='Number of workers used in data-loading')
+parser.add_argument('--epochs', default=70, type=int, help='Number of training epochs')
+parser.add_argument('--cuda', dest='cuda', action='store_true', help='Use cuda to train model')
+parser.add_argument('--lr', '--learning-rate', default=3e-4, type=float, help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+parser.add_argument('--learning-anneal', default=1.1, type=float, help='Annealing applied to learning rate every epoch')
+parser.add_argument('--silent', dest='silent', action='store_true', help='Turn off progress tracking per iteration')
+parser.add_argument('--checkpoint', dest='checkpoint', action='store_true', help='Enables checkpoint saving of model')
+parser.add_argument('--checkpoint-per-batch', default=0, type=int, help='Save checkpoint per batch. 0 means never save')
+parser.add_argument('--visdom', dest='visdom', action='store_true', help='Turn on visdom graphing')
+parser.add_argument('--tensorboard', dest='tensorboard', action='store_true', help='Turn on tensorboard graphing')
+parser.add_argument('--log-dir', default='visualize/capsnet_final', help='Location of tensorboard log')
+parser.add_argument('--log-params', dest='log_params', action='store_true', help='Log parameter values and gradients')
+parser.add_argument('--save-folder', default='models/', help='Location to save epoch models')
+parser.add_argument('--model-path', default='models/capsnet_final.pth',
+                    help='Location to save best validation model')
+parser.add_argument('--continue-from', default='', help='Continue from checkpoint model')
+parser.add_argument('--finetune', dest='finetune', action='store_true',
+                    help='Finetune the model from checkpoint "continue_from"')
+parser.add_argument('--no-shuffle', dest='no_shuffle', action='store_true',
+                    help='Turn off shuffling and sample from dataset based on sequence length (smallest to largest)')
+parser.add_argument('--dist-backend', default='gloo', type=str, help='distributed backend')
+parser.add_argument('--world-size', default=1, type=int,
+                    help='number of distributed processes')
+parser.add_argument('--rank', default=0, type=int,
+                    help='The rank of this process')
+parser.add_argument('--gpu-rank', default=None,
+                    help='If using distributed parallel for multi-gpu, sets the GPU for the process')
+args = parser.parse_args()
+
+
+data_transformer = transforms.Compose(
+					[transforms.Resize(size=(args.img_height, args.img_width), interpolation=2) # PIL.Image.BILINEAR
+					 transforms.ToTensor(),
+					 transforms.Normalize(mean=(0.5,), std=(0.5, ))]) # normalize in [-1, 1]
